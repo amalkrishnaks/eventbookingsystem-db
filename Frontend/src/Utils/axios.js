9b34a34e681c92f701a68774fa9886de43b2aa5b
AMAL KRISHNA K. S <98804382+amalkrishnaks@users.noreply.github.com>
2025-05-21 12:00:13 +0530

    Update axios.js

--- Frontend/src/Utils/axios.js
+++ Frontend/src/Utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance=axios.create({
-    baseURL:'http://localhost:4000/api/',
+    baseURL:'eventbookingsystem-db.vercel.app
+/api/',
     timeout:5000,
 });
 
