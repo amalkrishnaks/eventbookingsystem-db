5f230c7e3a82143c68db23af9f9348db04bad858
Amal <amal@gmail.com>
2025-06-06 12:22:39 +0530

    changes in the event route

--- Frontend/src/Utils/axios.js
+++ Frontend/src/Utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance=axios.create({
       // baseURL:'http://localhost:4000/api/',
-    baseURL:"https://eventbookingsystem-db.onrender.com/api/",
+    baseURL:"https://eventbookingsystem-db.onrender.com",
     timeout:5000,
 });
 
