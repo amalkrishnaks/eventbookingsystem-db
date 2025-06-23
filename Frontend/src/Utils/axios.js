f68b32ad95f774b01566a72690015df495e2597e
Amal <amal@gmail.com>
2025-06-24 01:25:37 +0530

    added  timeout

--- Frontend/src/Utils/axios.js
+++ Frontend/src/Utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
  const instance=axios.create({
       baseURL:'https://eventbookingsystem-db.onrender.com/api',
-    timeout:5000,
+    timeout:20000,
 });
 
 export default instance;
