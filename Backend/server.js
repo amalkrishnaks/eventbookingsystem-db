d72e7155909e8575281fc75bd672decdb498231a
Amal <amal@gmail.com>
2025-06-24 00:07:00 +0530

    changes in mongodb and env

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const cors=require('cors');
 const env=require( 'dotenv/config');
 const app=express();
-const PORT=process.env.PORT || 4000
+const PORT=process.env.PORT || 4000 ;
 
 app.use(cors());
 app.use(express.json());
